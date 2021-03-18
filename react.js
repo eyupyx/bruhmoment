324a36de03263450bc0aa911b49a7d4281250034
eyupyx <62266430+eyupyx@users.noreply.github.com>
2021-03-18 16:35:09 +0300

    Create react.js

---
+++ react.js
@@ -0,0 +1,7 @@
+// Adding reaction-role function
+client.on('messageReactionAdd', async (reaction, user) => {
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+  if (user.bot) return;
+  if (!reaction.message.guild) return;
+});
